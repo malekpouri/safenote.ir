263856545aee295f36d00fead7cfc5ad182b2d65
malekpouri <m.malekpoor@gmail.com>
2025-11-24 11:14:17 +0330

    chore: install frontend dependencies.

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"status": "ok",
+		})
+	})
+
+	log.Fatal(app.Listen(":8080"))
+}
